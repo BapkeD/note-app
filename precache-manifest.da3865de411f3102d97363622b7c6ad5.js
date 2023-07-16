53809f36c47651be6424433cf067e1ca23346664
BapkeD <baptisteturpin2009@gmail.com>
2023-07-16 13:04:14 +0200

    Updates

---
+++ precache-manifest.da3865de411f3102d97363622b7c6ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bda1432b000dbb08f1ba",
+    "url": "/note-app/static/css/main.a88d1070.chunk.css"
+  },
+  {
+    "revision": "bda1432b000dbb08f1ba",
+    "url": "/note-app/static/js/main.bda1432b.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/note-app/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "b23d4991278990232924",
+    "url": "/note-app/static/js/2.b23d4991.chunk.js"
+  },
+  {
+    "revision": "5fab4f7ce54bf7a4972a",
+    "url": "/note-app/static/js/runtime~main.5fab4f7c.js"
+  },
+  {
+    "revision": "b0a905cd7dfae05b077ca282e7c1c54b",
+    "url": "/note-app/index.html"
+  }
+];
